26b40adc7dadd192af8bcf109a3cb0aa34986edc
baligh-p <belighzoughlemi8@gmail.com>
2022-06-18 12:38:12 +0100

    version after nav

--- src/components/test/Test.jsx
+++ src/components/test/Test.jsx
@@ -1,119 +1,145 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Panier from "./Panier"
 const Test = () => {
+    const [toggleMenu, setToggleMenu] = useState(false)
+    const handleShowChilds = (e) => {
+        const thchild = e.target.parentNode.childNodes[2]
+        if (!e.target.classList.contains("rotate-90")) {
+            e.target.classList.add("rotate-90")
+        } else {
+            e.target.classList.remove("rotate-90")
+        }
+        if (thchild.style.display == "flex") {
+            thchild.style.display = "none"
+        }
+        else {
+            thchild.style.display = "flex"
+        }
+    }
     return (
-        <div class="header_middle">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-3 col-md-6">
-                        <div class="logo">
-                            <a href="index.html"><img src="/temp/assets/img/logo/logo.png" alt="" /></a>
-                        </div>
-                    </div>
-                    <div class="col-lg-9 col-md-6">
-                        <div class="middel_right">
-                            <div class="search_container">
-                                <form action="#">
-                                    <div class="hover_category">
-                                        <select class="select_option" name="select" id="categori1">
-                                            <option selected value="1">All Categories</option>
-                                            <option value="2">Accessories</option>
-                                            <option value="3">Accessories & More</option>
-                                            <option value="4">Butters & Eggs</option>
-                                            <option value="5">Camera & Video </option>
-                                            <option value="6">Mornitors</option>
-                                            <option value="7">Tablets</option>
-                                            <option value="8">Laptops</option>
-                                            <option value="9">Handbags</option>
-                                            <option value="10">Headphone & Speaker</option>
-                                            <option value="11">Herbs & botanicals</option>
-                                            <option value="12">Vegetables</option>
-                                            <option value="13">Shop</option>
-                                            <option value="14">Laptops & Desktops</option>
-                                            <option value="15">Watchs</option>
-                                            <option value="16">Electronic</option>
-                                        </select>
-                                    </div>
-                                    <div class="search_box">
-                                        <input placeholder="Search product..." type="text" />
-                                        <button type="submit">Search</button>
-                                    </div>
-                                </form>
+        <div>
+            <div class="header_middle">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-3 col-md-6">
+                            <div class="logo">
+                                <a href="index.html"><img src="/temp/assets/img/logo/logo.png" alt="" /></a>
                             </div>
-                            <div class="middel_right_info">
-                                <Panier />
+                        </div>
+                        <div class="col-lg-9 col-md-6">
+                            <div class="middel_right">
+                                <div class="search_container">
+                                    <form action="#">
+                                        <div class="hover_category">
+                                            <select class="select_option" name="select" id="categori1">
+                                                <option selected value="1">All Categories</option>
+                                                <option value="2">Accessories</option>
+                                                <option value="3">Accessories & More</option>
+                                                <option value="4">Butters & Eggs</option>
+                                                <option value="5">Camera & Video </option>
+                                                <option value="6">Mornitors</option>
+                                                <option value="7">Tablets</option>
+                                                <option value="8">Laptops</option>
+                                                <option value="9">Handbags</option>
+                                                <option value="10">Headphone & Speaker</option>
+                                                <option value="11">Herbs & botanicals</option>
+                                                <option value="12">Vegetables</option>
+                                                <option value="13">Shop</option>
+                                                <option value="14">Laptops & Desktops</option>
+                                                <option value="15">Watchs</option>
+                                                <option value="16">Electronic</option>
+                                            </select>
+                                        </div>
+                                        <div class="search_box">
+                                            <input placeholder="Search product..." type="text" />
+                                            <button type="submit">Search</button>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="middel_right_info">
+                                    <Panier />
+                                    <div style={{ marginLeft: "40px" }}>name</div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div id="menu" class="text-left ">
-                            <ul class="offcanvas_main_menu">
-                                <li class="menu-item-has-children active">
-                                    <a href="#">Home</a>
-                                </li>
-                                <li class="menu-item-has-children">
-                                    <a href="#">Shop</a>
-                                    <ul class="sub-menu">
-                                        <li class="menu-item-has-children">
-                                            <a href="#">Shop Layouts</a>
-                                            <ul class="sub-menu">
-                                                <li><a href="shop.html">shop</a></li>
-                                                <li><a href="shop-fullwidth.html">Full Width</a></li>
-                                                <li><a href="shop-fullwidth-list.html">Full Width list</a></li>
-                                                <li><a href="shop-right-sidebar.html">Right Sidebar </a></li>
-                                                <li><a href="shop-right-sidebar-list.html"> Right Sidebar list</a></li>
-                                                <li><a href="shop-list.html">List View</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="menu-item-has-children">
-                                            <a href="#">other Pages</a>
-                                            <ul class="sub-menu">
-                                                <li><a href="cart.html">cart</a></li>
-                                                <li><a href="wishlist.html">Wishlist</a></li>
-                                                <li><a href="checkout.html">Checkout</a></li>
-                                                <li><a href="my-account.html">my account</a></li>
-                                                <li><a href="404.html">Error 404</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="menu-item-has-children">
-                                            <a href="#">Product Types</a>
-                                            <ul class="sub-menu">
-                                                <li><a href="product-details.html">product details</a></li>
-                                                <li><a href="product-sidebar.html">product sidebar</a></li>
-                                                <li><a href="product-grouped.html">product grouped</a></li>
-                                                <li><a href="variable-product.html">product variable</a></li>
-                                                <li><a href="product-countdown.html">product countdown</a></li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="menu-item-has-children">
-                                    <a href="#">pages </a>
-                                    <ul class="sub-menu">
-                                        <li><a href="about.html">About Us</a></li>
-                                        <li><a href="services.html">services</a></li>
-                                        <li><a href="privacy-policy.html">privacy policy</a></li>
-                                        <li><a href="faq.html">Frequently Questions</a></li>
-                                        <li><a href="contact.html">contact</a></li>
-                                        <li><a href="login.html">login</a></li>
-                                        <li><a href="404.html">Error 404</a></li>
-                                        <li><a href="compare.html">Compare</a></li>
-                                    </ul>
-                                </li>
-                                <li class="menu-item-has-children">
-                                    <a href="my-account.html">my account</a>
-                                </li>
-                                <li class="menu-item-has-children">
-                                    <a href="about.html">about Us</a>
-                                </li>
-                                <li class="menu-item-has-children">
-                                    <a href="contact.html"> Contact Us</a>
-                                </li>
-                            </ul>
+            {/*categorie*/}
+            <div class="main_menu_area">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-3 col-md-12">
+                            <div class="categories_menu select-none">
+                                <div onClick={() => { setToggleMenu(!toggleMenu) }} class="categories_title">
+                                    <h2 class="">ALL CATEGORIES</h2>
+                                </div>
+                                <div className={` ${toggleMenu ? "py-3 h-auto" : "max-h-0 overflow-y-hidden"} w-full space-y-3 px-8 bg-white absolute border border-stone-200 text-neutral-900 `}>
+                                    <div className='flex w-full flex-wrap items-center'>
+                                        <div className='cursor-pointer hover:text-blue-500 hover:scale-105 hover:text-blue' >Video Games</div>
+                                        <p onClick={handleShowChilds} className='  cursor-pointer select-none ml-auto text-xl'>{">"}</p>
+                                        <div className='hidden flex-col ml-10 mt-1 space-y-2 flex-none w-full'>
+                                            <div className='flex hover:text-blue-500 cursor-pointer w-full items-center'>
+                                                <div className='hover:scale-105'>Video Games</div>
+                                            </div>
+                                            <div className='flex hover:text-blue-500 cursor-pointer w-full items-center'>
+                                                <div className='hover:scale-105'>Video Games</div>
+                                            </div>
+                                            <div className='flex hover:text-blue-500 cursor-pointer w-full items-center'>
+                                                <div className='hover:scale-105'>Video Games</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='flex w-full flex-wrap items-center'>
+                                        <div className='cursor-pointer hover:text-blue-500 hover:scale-105 hover:text-blue' >Video Games</div>
+                                        <p onClick={handleShowChilds} className='  cursor-pointer select-none ml-auto text-xl'>{">"}</p>
+                                        <div className='hidden flex-col ml-10 mt-1 space-y-2 flex-none w-full'>
+                                            <div className='flex hover:text-blue-500 cursor-pointer w-full items-center'>
+                                                <div className='hover:scale-105'>Video Games</div>
+                                            </div>
+                                            <div className='flex hover:text-blue-500 cursor-pointer w-full items-center'>
+                                                <div className='hover:scale-105'>Video Games</div>
+                                            </div>
+                                            <div className='flex hover:text-blue-500 cursor-pointer w-full items-center'>
+                                                <div className='hover:scale-105'>Video Games</div>
+                                            </div>
+                                        </div>
+                                    </div><div className='flex w-full flex-wrap items-center'>
+                                        <div className='cursor-pointer hover:text-blue-500 hover:scale-105 hover:text-blue' >Video Games</div>
+                                        <p onClick={handleShowChilds} className='  cursor-pointer select-none ml-auto text-xl'>{">"}</p>
+                                        <div className='hidden flex-col ml-10 mt-1 space-y-2 flex-none w-full'>
+                                            <div className='flex hover:text-blue-500 cursor-pointer w-full items-center'>
+                                                <div className='hover:scale-105'>Video Games</div>
+                                            </div>
+                                            <div className='flex hover:text-blue-500 cursor-pointer w-full items-center'>
+                                                <div className='hover:scale-105'>Video Games</div>
+                                            </div>
+                                            <div className='flex hover:text-blue-500 cursor-pointer w-full items-center'>
+                                                <div className='hover:scale-105'>Video Games</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        </div >
+                        <div class="col-lg-9 col-md-12">
+                            <div class="main_menu menu_position">
+                                <nav>
+                                    <ul>
+                                        <li><a class="active" href="index.html">home<i class="fa"></i></a>
+                                        </li>
+                                        <li class="mega_items"><a href="shop.html">shop</a>
+                                        </li>
+                                        <li><a href="about.html">about Us</a></li>
+                                        <li><a href="contact.html"> Contact Us</a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div >
     )
 }
 
